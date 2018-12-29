81e1355f12f27db9f85464d353e1d51536532948
Ivan Mihaylov <ivan1mihaylov@hotmail.com>
2018-12-29 22:27:42 +0200

    Async database change and TextMesh import

--- Assets/Scripts/scounte.cs
+++ Assets/Scripts/scounte.cs
@@ -10,37 +10,54 @@ public class scounte : MonoBehaviour {
 
 	int count;
 	public Text countText;
+	bool isFirebaseAvaible;
 
 	// Use this for initialization
 	void Start () {
 
+		Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+			var dependencyStatus = task.Result;
+			if (dependencyStatus == Firebase.DependencyStatus.Available) {
+				isFirebaseAvaible = true;
+			} else {
+				UnityEngine.Debug.LogError(System.String.Format(
+					"Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+				isFirebaseAvaible = false;
+			}
+		});
 
+		if (isFirebaseAvaible) {
+			FirebaseApp.DefaultInstance.SetEditorDatabaseUrl ("https://fir-test-5d377.firebaseio.com");
 
-		FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://fir-test-5d377.firebaseio.com");
+			DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
 
-		DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
+			count = 0;
+			//SetCountText (); 
 
-		count = 0;
-		//SetCountText (); 
+			//FirebaseDatabase.DefaultInstance
+			//	.GetReference("/counters/NL3vgzJOz7Mufhz1ZenG/count")
+			//	.ValueChanged += HandleValueChanged;
 
-		//FirebaseDatabase.DefaultInstance
-		//	.GetReference("/counters/NL3vgzJOz7Mufhz1ZenG/count")
-		//	.ValueChanged += HandleValueChanged;
-
-		FirebaseDatabase.DefaultInstance
-			.GetReference("counter")
-			.GetValueAsync().ContinueWith(task => {
+			FirebaseDatabase.DefaultInstance
+			.GetReference ("counter")
+			.GetValueAsync ().ContinueWith (task => {
 				if (task.IsFaulted) {
 					// Handle the error...
-				}
-				else if (task.IsCompleted) {
+				} else if (task.IsCompleted) {
 					DataSnapshot snapshot = task.Result;
 					// Do something with snapshot...
-					Debug.Log(snapshot);
-					countText.text = snapshot.Value.ToString();
+					Debug.Log (snapshot);
+					countText.text = snapshot.Value.ToString ();
+
+						FirebaseDatabase.DefaultInstance
+							.GetReference("counter")
+							.ValueChanged += HandleValueChanged;
 
 				}
 			});
+		} else {
+			countText.text = "Error in Firebase dependencies";
+		}
 	}
 
 
@@ -55,6 +72,15 @@ public class scounte : MonoBehaviour {
 	}
 
 	void SetCountText(){
-		countText.text = count.ToString ();
+		countText.text = count.ToString();
+	}
+
+	void HandleValueChanged(object sender, ValueChangedEventArgs args) {
+		if (args.DatabaseError != null) {
+			Debug.LogError(args.DatabaseError.Message);
+			return;
+		}
+
+		countText.text = args.Snapshot.Value.ToString();
 	}
 }
